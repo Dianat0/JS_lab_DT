7edc99ee46caa3c59ba898ffff739f63f84c345f
Dianat0 <diana.taras.24@pnu.edu.ua>
2026-09-10 11:34:39 +0300

    Update script.js

--- lab1/script.js
+++ lab1/script.js
@@ -33,8 +33,8 @@ function triangle(val1, type1, val2, type2) {
     else if ((type1 === "leg" && type2 === "adjacent angle") || (type1 === "adjacent angle" && type2 === "leg")) {
         if (type1 === "leg") { a = val1; beta = val2; } 
         else { a = val2; beta = val1; }
-        if (beta <= 0 || beta >= 90) 
-            return "Кут повинен бути гострим (від 0 до 90 градусів)";
+        if (beta <= 0.000001 || beta >= 89.999999)
+            return "Кут занадто близький до 0° або 90°, можливе виникнення похибки";
         alpha = 90 - beta;
         b = a * Math.tan(rad(beta));
         c = a / Math.cos(rad(beta));
@@ -43,8 +43,8 @@ function triangle(val1, type1, val2, type2) {
     else if ((type1 === "leg" && type2 === "opposite angle") || (type1 === "opposite angle" && type2 === "leg")) {
         if (type1 === "leg") { a = val1; alpha = val2; } 
         else { a = val2; alpha = val1; }
-        if (alpha <= 0 || alpha >= 90)
-            return "Кут повинен бути гострим (від 0 до 90 градусів)";
+        if (alpha <= 0.000001 || alpha >= 89.999999)
+            return "Кут занадто близький до 0° або 90°, можливе виникнення похибки";
         beta = 90 - alpha;
         b = a / Math.tan(rad(alpha));
         c = a / Math.sin(rad(alpha));
@@ -53,8 +53,8 @@ function triangle(val1, type1, val2, type2) {
     else if ((type1 === "hypotenuse" && type2 === "angle") || (type1 === "angle" && type2 === "hypotenuse")) {
         if (type1 === "hypotenuse") { c = val1; alpha = val2; } 
         else { c = val2; alpha = val1; }
-        if (alpha <= 0 || alpha >= 90) 
-            return "Кут повинен бути гострим (від 0 до 90 градусів)";
+        if (alpha <= 0.000001 || alpha >= 89.999999)
+            return "Кут занадто близький до 0° або 90°, можливе виникнення похибки";
         beta = 90 - alpha;
         a = c * Math.sin(rad(alpha));
         b = c * Math.cos(rad(alpha));
