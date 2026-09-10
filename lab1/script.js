0eac3de0f556b5e99c365ffdd304dafdb30def19
Dianat0 <diana.taras.24@pnu.edu.ua>
2026-09-10 18:02:40 +0300

    Update script.js

--- lab1/script.js
+++ lab1/script.js
@@ -15,16 +15,19 @@ function triangle(val1, type1, val2, type2) {
     if (type1 === "leg" && type2 === "leg") {
         a = val1;
         b = val2;
+        if (a <= 0.000001 || b <= 0.000001) {
+           return "Увага: катет занадто малий, можливе виникнення похибки";
+    }
         c = Math.sqrt(a * a + b * b);
         alpha = Math.atan(a / b) * (180 / Math.PI);
         beta = 90 - alpha;
     }
-    // 2. Катет і гіпотенуза
+   // 2. Катет і гіпотенуза
     else if ((type1 === "leg" && type2 === "hypotenuse") || (type1 === "hypotenuse" && type2 === "leg")) {
         if (type1 === "leg") { a = val1; c = val2; } 
         else { a = val2; c = val1; }
         if (a >= c - 0.000001) {
-            return "Увага: катет занадто близький до гіпотенузи, можливе виникнення похибки.";
+            return "Увага: катет занадто близький до гіпотенузи, можливе виникнення похибки";
         }
         b = Math.sqrt(c * c - a * a);
         alpha = Math.asin(a / c) * (180 / Math.PI);
